284453303962493385509ffc37bf2d20b3e8bf87
molly soufrine <mollysoufrine@gmail.com>
2020-09-10 12:10:23 -0400

    renamed the .png files of the pumpkins

--- index.js
+++ index.js
@@ -19,4 +19,6 @@ roundScore = 0;
 activePlayer = 0;
 
 pumpkin = Math.floor(Math.random * 6) + 1;
-console.log(pumpkin);
+// console.log(pumpkin);
+
+document.querySelector("#score-0").textContent = pumpkin;
