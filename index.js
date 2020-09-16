58fdf8fc3eecf5f07f18d3f1b094a67eb9b861e3
molly soufrine <mollysoufrine@gmail.com>
2020-09-16 08:22:09 -0400

    was missing an else statement on line 78 to wrap around nectPlayer(). also added a car in global scope called gamePlaying. Wrapped an if statement with that var around btn-roll so we can tell that it is true the game is playing. added it in the if statement that detects if someone won and set it to false there so we know the game is over. this way a player can not roll the dice more after the game has ended

--- index.js
+++ index.js
@@ -12,30 +12,39 @@ above are the originl rules of the game from the udemy course i followed about j
 to chanfe the photos from dice to pumpkins and depending on which face comes up, it will add points the user will lose all
 their points and it will change to the other user's turn. Still the same rules, just changing the photos really.
 */
-var scores, roundScore, activePlayer;
+var scores, roundScore, activePlayer, gamePlaying;
+//declare a state var tha tells us whether the game is being played or not..gamePlaying
+//decalred in global scope to be used in other functions
 
 //call init function
 init();
 
 document.querySelector(".btn-roll").addEventListener("click", function () {
-  //when someone clicks the button, we need a random number
-  var nightmare = Math.floor(Math.random() * 6) + 1;
-  // console.log(pumpkin);
-  //then we need to display result
-  var nightmareDOM = document.querySelector(".nightmare");
-  nightmareDOM.style.display = "block";
-  nightmareDOM.src = "nightmare-" + nightmare + ".png";
+  if (gamePlaying) {
+    //when someone clicks the button, we need a random number
+    var nightmare = Math.floor(Math.random() * 6) + 1;
+    // console.log(pumpkin);
+    //then we need to display result
+    var nightmareDOM = document.querySelector(".nightmare");
+    nightmareDOM.style.display = "block";
+    nightmareDOM.src = "nightmare-" + nightmare + ".png";
 
-  //Update the round score but only if the rolled pumpkin is not pumpkin1
-  if (nightmare !== 1) {
-    //add score
-    roundScore += nightmare;
-    //first update roundScore
-    document.querySelector("#current-" + activePlayer).textContent = roundScore;
-    //then display roundScore
-  } else {
-    nextPlayer();
+    //Update the round score but only if the rolled pumpkin is not pumpkin1
+    if (nightmare !== 1) {
+      //add score
+      roundScore += nightmare;
+      //first update roundScore
+      document.querySelector(
+        "#current-" + activePlayer
+      ).textContent = roundScore;
+      //then display roundScore
+    } else {
+      nextPlayer();
+    }
   }
+  //we only need the var because the if statement returns a true or false value
+  //we then wrap it around our code, so if gamePlaying is true then do this
+  //once gamePlaying is false, which mean the game is over, then you can no longer roll the "dice"
 });
 
 document.querySelector(".btn-hold").addEventListener("click", function () {
@@ -63,9 +72,12 @@ document.querySelector(".btn-hold").addEventListener("click", function () {
     document
       .querySelector(".player-" + activePlayer + "-panel")
       .classList.remove("winner");
+    gamePlaying = false;
+    //we set gamePlaying to false here, after a winner has been detected to tell
+    //that the game is over
+  } else {
+    nextPlayer();
   }
-
-  nextPlayer();
 });
 
 //implement DRY
@@ -115,5 +127,3 @@ function init() {
   //then add it back to the first one
   document.querySelector(".player-0-panel").classList.add("active");
 }
-
-//the abover was used to test something but may need it later
